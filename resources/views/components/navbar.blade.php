802b20e854d7e5bab805bd8276515dfbbd314024
Hakimrizm <hakimrizm05@gmail.com>
2024-08-07 02:06:27 +0700

    1-2 Merevisi landing page

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary shadow-sm sticky-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="{{ asset("icon/logo.png") }}" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Pasang Lowongan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cari Lowogan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tips Loker</a>
+        </li>
+      </ul>
+      <div class="d-flex">
+        <button type="button" class="btn btn-primary me-2">Register</button>
+        <button class="btn btn-outline-success" type="submit">Login</button>
+      </div>
+    </div>
+  </div>
+</nav>
