9c50bec218472ece3739748f038ae2f60ed68ff8
Hakimrizm <hakimrizm05@gmail.com>
2024-08-07 12:53:50 +0700

    menambahkan login

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -23,8 +23,39 @@
       </ul>
       <div class="d-flex">
         <button type="button" class="btn btn-primary me-2">Register</button>
-        <button class="btn btn-outline-success" type="submit">Login</button>
+        <button class="btn btn-outline-success" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">Login</button>
       </div>
     </div>
   </div>
 </nav>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5 text-center" id="exampleModalLabel">Login</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" class="btn btn-primary">Login</button>
+          <p>Dont have an account? <a href="">Register</a></p>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
